0300c308499e792cbb20fae7cf870633079b0672
Henrique Ferreira Cardoso <129521477+HenriqueFerC@users.noreply.github.com>
2025-05-23 13:06:35 -0300

    subo a estrutura

---
+++ work/work.js
@@ -0,0 +1,119 @@
+const sistemaFluxo = document.querySelector("#sistemaFluxo");
+
+sistemaFluxo.addEventListener("click", function(){
+    window.open("https://sistemafluxo.dasa.com.br/v1/tracking/t1/patients");
+})
+
+const gliese = document.querySelector("#gliese");
+
+gliese.addEventListener("click", function(){
+    window.open("https://atende.dasa.com.br")
+})
+
+const gliesePiloto = document.querySelector("#gliesePiloto");
+
+gliesePiloto.addEventListener("click", function(){
+    window.open("https://atendepiloto.dasa.com.br/");
+});
+
+const oraculo = document.querySelector("#oraculo");
+
+oraculo.addEventListener("click", function(){
+    window.open("https://oraculo.dasa.com.br/");
+});
+
+const teams = document.querySelector("#teams");
+
+teams.addEventListener("click", function(){
+    window.open("https://teams.microsoft.com/v2/");
+});
+
+const sqDasa = document.querySelector("#sqDasa");
+
+sqDasa.addEventListener("click", function(){
+    window.open("https://sq.dasa.com.br/");
+});
+
+const canalDoConsultor = document.querySelector("#canalDoConsultor");
+
+canalDoConsultor.addEventListener("click", function(){
+    window.open("https://canaldoconsultor.dasa.com.br");
+});
+
+const orizon = document.querySelector("#orizon");
+
+orizon.addEventListener("click", function(){
+    window.open("https://polimed.com.br/autenticadorOrizon/loginAutenticador");
+});
+
+const sulamerica = document.querySelector("#sulamericabotao");
+
+sulamerica.addEventListener("click", function(){
+    window.open("https://saude.sulamericaseguros.com.br/prestador/login/?accessError=2");
+});
+
+const bradesco = document.querySelector("#bradescobotao");
+
+bradesco.addEventListener("click", function(){
+    window.open("https://wwws.bradescosaude.com.br/PCBS-GerenciadorPortal/td/loginReferenciado.do");
+});
+
+const amil = document.querySelector("#amilbotao");
+
+amil.addEventListener("click", function(){
+    window.open("https://credenciado.amil.com.br/login?action=session")
+})
+
+const saviatendimento = document.querySelector("#saviatendimentobotao");
+
+saviatendimento.addEventListener("click", function(){
+    window.open("https://saviatendimento.com.br/");
+});
+
+const portoSaude = document.querySelector("#portoSaudebotao");
+
+portoSaude.addEventListener("click", function(){
+    window.open("https://prestadores.portosaude.com.br/portal-prestador/");
+});
+
+const unimed = document.querySelector("#unimedbotao");
+
+unimed.addEventListener("click", () => {
+    window.open("https://saw.trixti.com.br/saw/Logar.do?method=abrirSAW");
+});
+
+const segurosUnimed = document.querySelector("#segurosUnimedbotao");
+
+segurosUnimed.addEventListener("click", () => {
+    window.open("https://topsaude.segurosunimed.com.br/TSNMVC/Account/Login?returnUrl=/TSNMVC/TSNMVC/Home/AreaLogada");
+});
+
+const postalSaude = document.querySelector("#postalSaudebotao");
+
+postalSaude.addEventListener("click", () => {
+    window.open("https://autorizador.postalsaudeservicos.com.br/autorizadorpro/custom/CustomLogin.aspx");
+});
+
+const blue = document.querySelector("#bluebotao");
+
+blue.addEventListener("click", () => {
+    window.open("https://planoonline.com.br/autorizadorweb_v2/blue");
+});
+
+const labet = document.querySelector("#labetbotao");
+
+labet.addEventListener("click", () => {
+    window.open("https://rede.labest.com.br/auth");
+});
+
+const geap = document.querySelector("#geapbotao");
+
+geap.addEventListener("click", () => {
+    window.open("https://login.geap.com.br/account/signin?p=%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dgeap.portal.internet.prestador%26redirect_uri%3Dhttps%253A%252F%252Fsisgeap.geap.com.br%252Fportalprestador%252Flogin-callback%26response_type%3Dcode%26scope%3Dopenid%2520profile%25201064_S%25201064_I%25201064_U%25201064_D%252037_E%252016_E%26state%3D2b0f0d36c8004e9d82b2772850f1faa8%26code_challenge%3DvYGUONPBEwqBcqxIvfqDn5oPdheAj8a3KOJfe11nxEs%26code_challenge_method%3DS256%26response_mode%3Dquery");
+});
+
+const gama = document.querySelector("#gamabotao");
+
+gama.addEventListener("click", () => {
+    window.open("https://wwwt.connectmed.com.br/saudeweb/seguranca/login");
+});
